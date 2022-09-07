70b3721fcfbec25b521d53b4414126eda3be7009
Wesley DuSell <dusellfam@gmail.com>
2022-09-06 23:22:10 -0400

    Copy needed components and scss theme files.

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -5,14 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+import { NavbarModule } from './components/navbar/navbar.module';
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    NavbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
