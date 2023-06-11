4a415ed8781896ecb43c2ee14eaaa52746a95973
Wesley DuSell <dusellfam@gmail.com>
2023-06-11 16:15:26 -0400

    Upgrade Material to 15

--- projects/demo/src/app/pages/home/home.module.ts
+++ projects/demo/src/app/pages/home/home.module.ts
@@ -3,8 +3,8 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
+import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
+import { MatLegacyCardModule as MatCardModule } from '@angular/material/legacy-card';
 import { MatDividerModule } from '@angular/material/divider';
 import { TerryLibModule } from 'terry-lib';
 
